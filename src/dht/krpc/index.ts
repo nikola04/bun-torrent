7662dfb1e8085a2b5b8584be017854a6500e8ad4
Nikola Nedeljkovic <nikolanedeljkovic.official@gmail.com>
2026-05-23 21:15:57 +0200

    feat: krpc exports

---
+++ src/dht/krpc/index.ts
@@ -0,0 +1,3 @@
+export { decodeKrpcMessage, encodeKrpcMessage } from './messages';
+export { KrpcMessageType, KrpcQueryType } from './types';
+export type { KrpcError, KrpcMessage, KrpcQuery, KrpcResponse, KrpcTransactionId } from './types';
